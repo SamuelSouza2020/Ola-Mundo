207c26b0957ebdc1ae590b8f468c1c772485e156
Samuel Souza <souzasamuel623@gmail.com>
2021-03-21 14:14:30 -0300

    Codigo C Sharp Teste

---
+++ Teste C SHARP/teste.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int x = 20;
+
+            Console.WriteLine("O valor de X: " + x.ToString());
+            Console.ReadLine();
+        }
+    }
+}
